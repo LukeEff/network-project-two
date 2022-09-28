d1c7e8da6a5621144241f8e6d6d233f30cf5815e
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-09-28 12:53:00 -0700

    add missing response

--- webserver.py
+++ webserver.py
@@ -10,7 +10,7 @@ s.listen()
 
 while True:
     new_conn = s.accept()
-    print (new_conn)
+    print(new_conn)
     new_socket = new_conn[0]
     req = new_socket.recv(4096)
     while True:
@@ -20,5 +20,11 @@ while True:
         if req_text.find('\r\n\r\n'):
             break
         req = new_socket.recv(4096)
+    resp = 'HTTP/1.1 200 OK\n\
+Content-Type: text/plain\n\
+Content-Length: 6\n\
+Connection: close\n\
+Hello!'.encode("ISO-8859-1")
+    new_socket.sendall(resp)
     print("Found end of line. Closing new socket")
     new_socket.close()
